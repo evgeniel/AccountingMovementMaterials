df74896577b848ca24197449682e1ee03c037905
vasiyarov-evgeniy <vasiyarov-evgeniy@mail.ru>
2017-06-07 16:29:53 +0400

    Project

---
+++ UchetPeremesheniaMaterialov/PlanSchetov.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace UchetPeremesheniaMaterialov
+{
+    public partial class PlanSchetov : Form
+    {
+        private SQLiteConnection con;
+        private SQLiteCommand cmd;
+        private DataTable dt;
+
+        public PlanSchetov()
+        {
+            InitializeComponent();
+        }
+
+        private void PlanSchetov_Load(object sender, EventArgs e)
+        {
+            con = new SQLiteConnection();
+            con.ConnectionString = @"Data Source=C:\Users\EvgenieL\Documents\Visual Studio 2013\Projects\UchetPeremesheniaMaterialov\UchetPeremesheniaMaterialov\mybd.db;New=False;Version=3";
+
+            cmd = new SQLiteCommand();
+            cmd.Connection = con;
+
+            dt = new DataTable();
+            dataGridView1.DataSource = dt; // связываешь DataTable и таблицу на форме
+
+            con.Open(); // открываешь соединение с БД
+            cmd.CommandText = "Select * from PlanSchetov";
+            dt.Clear();
+            dt.Load(cmd.ExecuteReader()); // выполняешь SQL-запрос
+            con.Close(); // закрываешь соединение с БД
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+    }
+
+}
---
+++ UchetPeremesheniaMaterialov/UchetMaterialov.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace UchetPeremesheniaMaterialov
+{
+    partial class UchetMaterialov
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.справочникиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.мToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.мОЛToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.планСчетовToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.подразделениеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.журналПроводокToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отчетToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.справочникиToolStripMenuItem,
+            this.журналПроводокToolStripMenuItem,
+            this.отчетToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(588, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // справочникиToolStripMenuItem
+            // 
+            this.справочникиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.мToolStripMenuItem,
+            this.мОЛToolStripMenuItem,
+            this.планСчетовToolStripMenuItem,
+            this.подразделениеToolStripMenuItem});
+            this.справочникиToolStripMenuItem.Name = "справочникиToolStripMenuItem";
+            this.справочникиToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.справочникиToolStripMenuItem.Text = "Справочники";
+            // 
+            // мToolStripMenuItem
+            // 
+            this.мToolStripMenuItem.Name = "мToolStripMenuItem";
+            this.мToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.мToolStripMenuItem.Text = "Материалы";
+            // 
+            // мОЛToolStripMenuItem
+            // 
+            this.мОЛToolStripMenuItem.Name = "мОЛToolStripMenuItem";
+            this.мОЛToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.мОЛToolStripMenuItem.Text = "МОЛ";
+            // 
+            // планСчетовToolStripMenuItem
+            // 
+            this.планСчетовToolStripMenuItem.Name = "планСчетовToolStripMenuItem";
+            this.планСчетовToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.планСчетовToolStripMenuItem.Text = "План счетов";
+            this.планСчетовToolStripMenuItem.Click += new System.EventHandler(this.планСчетовToolStripMenuItem_Click);
+            // 
+            // подразделениеToolStripMenuItem
+            // 
+            this.подразделениеToolStripMenuItem.Name = "подразделениеToolStripMenuItem";
+            this.подразделениеToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.подразделениеToolStripMenuItem.Text = "Подразделение";
+            // 
+            // журналПроводокToolStripMenuItem
+            // 
+            this.журналПроводокToolStripMenuItem.Name = "журналПроводокToolStripMenuItem";
+            this.журналПроводокToolStripMenuItem.Size = new System.Drawing.Size(119, 20);
+            this.журналПроводокToolStripMenuItem.Text = "Журнал проводок";
+            // 
+            // отчетToolStripMenuItem
+            // 
+            this.отчетToolStripMenuItem.Name = "отчетToolStripMenuItem";
+            this.отчетToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.отчетToolStripMenuItem.Text = "Отчет";
+            // 
+            // UchetMaterialov
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 262);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "UchetMaterialov";
+            this.Text = "Учёт перемещения материалов";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem справочникиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem мToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem мОЛToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem планСчетовToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem подразделениеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem журналПроводокToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem отчетToolStripMenuItem;
+
+    }
+}
+
